706cd44bf9aa15b866ddf248f4f73650ed4a25a1
Thiago Rabelo <thiagorabelo1@gmail.com>
2018-04-15 17:32:31 -0300

    Teste funcional agora checa se podemos entrar com um item na lista To-Do

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
 
 
@@ -14,8 +17,29 @@ class NewVisitor(unittest.TestCase):
         self.brower.get('http://localhost:8000/')
 
         self.assertIn('To-Do', self.brower.title)
-        self.fail('Finish the test')
+        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        input_text = 'Compre penas de pavão'
+
+        inputbox.send_keys(input_text)
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == f'1: {input_text}' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
