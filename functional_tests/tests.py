cdc7fa5b81b2a071814ba3988fc3d2cbe7931802
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-04-24 18:34:37 -0300

    Testes funcionais usando tempo progressivo com um timeout no lugar de tempo fixo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,16 +5,12 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 
 class NewVisitor(LiveServerTestCase):
 
-    host = 'localhost'
-    port = 9000
-
-    @staticmethod
-    def sleep(seconds=1):
-        time.sleep(seconds)
+    MAX_WAIT = 10
 
     def setUp(self):
         self.brower = webdriver.Firefox()
@@ -27,10 +23,19 @@ class NewVisitor(LiveServerTestCase):
         inputbox.send_keys(text)
         inputbox.send_keys(Keys.ENTER)
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.brower.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as ex:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise ex
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):  # pylint: disable=C0103
         self.brower.get(self.live_server_url)
@@ -45,13 +50,11 @@ class NewVisitor(LiveServerTestCase):
 
         input_text_1 = 'Compre penas de pavão'
         self.submit_data_by_post(input_text_1)
-        self.sleep()
-        self.check_for_row_in_list_table(f'1: {input_text_1}')
+        self.wait_for_row_in_list_table(f'1: {input_text_1}')
 
         input_text_2 = 'Usar penas de pavão para fazer um fly'
         self.submit_data_by_post(input_text_2)
-        self.sleep()
-        self.check_for_row_in_list_table(f'1: {input_text_1}')
-        self.check_for_row_in_list_table(f'2: {input_text_2}')
+        self.wait_for_row_in_list_table(f'1: {input_text_1}')
+        self.wait_for_row_in_list_table(f'2: {input_text_2}')
 
         self.fail('Finish the test!')
