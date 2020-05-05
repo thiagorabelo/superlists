ad6fa35622b91f46f5ee18d594f58b27a7e18b2e
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-05 16:33:59 -0300

    Modelo ajustado para os itens sejam associados a listas distintas

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@
 
 from django.test import TestCase
 
-from .models import Item
+from .models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -12,18 +12,26 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         text_1 = 'O primeiro item (de todos) da lista'
         first_item.text = text_1
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         text_2 = 'O segundo Item'
         second_item.text = text_2
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -31,7 +39,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertCountEqual(first_saved_item.text, text_1)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertCountEqual(second_saved_item.text, text_2)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -41,8 +51,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
