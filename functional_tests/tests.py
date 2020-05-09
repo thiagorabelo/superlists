1b5db76af66e29066be87fcf4e805395c94ba565
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-09 00:40:30 -0300

    Hack no FT para permitir testes no servidor staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,6 +13,10 @@ class NewVisitor(StaticLiveServerTestCase, base.ExplicitWaitMixin):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = f'http://{staging_server}'
 
     def tearDown(self):
         self.browser.quit()
