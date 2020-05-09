8500f1df787b44ae1b262f049c9fbb740bcfc700
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-09 02:46:16 -0300

    Adicionado wait for ao testar posiçao do input

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -95,18 +95,23 @@ class NewVisitor(StaticLiveServerTestCase, base.ExplicitWaitMixin):
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
+
+        self.wait_for(
+            self.assertAlmostEqual,
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=10,
+            max_wait=3
         )
 
         text_1 = 'testando'
         self.submit_data_by_post(text_1)
         self.wait_for(self.check_for_row_in_list_table, f'1: {text_1}', max_wait=2)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
+        self.wait_for(
+            self.assertAlmostEqual,
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=10,
+            max_wait=3
         )
