3cceab4e5828baed225446157b063f3a75fafb4a
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-13 17:38:31 -0300

    Testes de unidade divididos em dois arquivos

---
+++ lists/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+
+from lists.models import List, Item
+
+
+class ListAndItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        text_1 = 'O primeiro item (de todos) da lista'
+        first_item.text = text_1
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        text_2 = 'O segundo Item'
+        second_item.text = text_2
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertCountEqual(first_saved_item.text, text_1)
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertCountEqual(second_saved_item.text, text_2)
+        self.assertEqual(second_saved_item.list, list_)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,7 @@
 
 from django.test import TestCase
 
-from .models import Item, List
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -10,38 +10,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
-
-
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        text_1 = 'O primeiro item (de todos) da lista'
-        first_item.text = text_1
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        text_2 = 'O segundo Item'
-        second_item.text = text_2
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertCountEqual(first_saved_item.text, text_1)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertCountEqual(second_saved_item.text, text_2)
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -72,7 +40,7 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, other_list_itemey_2)
 
     def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
+        other_list = List.objects.create()  # pylint: disable=unused-variable
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.pk}/')
 
