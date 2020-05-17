2f24b608f0c41948089bd02795822d64e6091b7a
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-16 23:57:25 -0300

    Novo formulário para itens de lista

---
+++ lists/tests/test_form.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_validation_form_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
