a812be7da2e01032d08e92a4286d5cbc9fc34b65
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-05-26 20:50:33 -0300

    Usando namespace para o código de inicialização javascripr

--- lists/static/js/lists.js
+++ lists/static/js/lists.js
@@ -1,5 +1,11 @@
-var initialize = function() {
-    $('input[name=text]').on('keypress', function(e) {
-        $('.has-error').hide();
-    });
-}
+(function(window) {
+    let superlists = window.Superlists || {};
+
+    superlists.initialize = () => {
+        $('input[name=text]').on('keypress', function(e) {
+            $('.has-error').hide();
+        });
+    };
+
+    window.Superlists = superlists;
+}(window));
