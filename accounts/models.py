848bab343d6de5099da7baa5fa6898cd63a7bea3
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-06-04 16:41:22 -0300

    Autendicação de usuário sem password funcional.

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(max_length=37, default=uuid.uuid4)
--- superlists/settings.py
+++ superlists/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'lists',
+    'accounts',
 ]
+
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +126,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'deploy/static'))
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        }
+    },
+    'root': {'level': 'INFO'}
+}
