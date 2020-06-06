7184d43bee12bdfebff2f386f89b815054f56d64
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-06-05 22:04:41 -0300

    Logout implementado

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
+from django.urls import reverse_lazy
 
 
 from . import views
@@ -9,4 +11,5 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^send_login_email/$', views.send_login_email, name='send_login_email'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', logout, {'next_page': reverse_lazy('home')}, name='logout'),
 ]
