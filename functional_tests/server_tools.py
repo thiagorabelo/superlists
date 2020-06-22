399ef57b9e26047078499a63dc685addea43f390
Thiago Rabelo <thiagorabelo1@gmail.com>
2020-06-22 00:24:15 -0300

    Testes de login no servidor de staging

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,4 +1,6 @@
-from fabric.api import run
+import os
+
+from fabric.api import run, env
 from fabric.context_managers import settings
 
 
@@ -15,6 +17,9 @@ def reset_database(host):
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'thiago@{host}'):
+    username = os.environ['HOST_USERNAME']
+    env.password = os.environ['HOST_PASSWORD']
+
+    with settings(host_string=f'{username}@{host}'):
         session_key = run(f'{manage_dot_py} create_session {email}')
         return session_key.strip()
